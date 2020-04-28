65d4cc461fa98c8e434b28c5d82e51b23a8387e3
ZaferSekerci <zafersekerci@ya.ru>
2020-04-29 02:40:21 +0300

    4 load cell work like one scale

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "sNilwAwLOEY69xx2LhQBw2QPDr8xMcpi";
+char auth[] = "sNilwAwLOEY69xx2LhQBw2QPDr8xMcpi"; //blynkden alınacak key
 BlynkTimer timer;
 
 // WiFi bilgileri.
@@ -24,17 +24,24 @@ char pass[] = "artemispardus*123";
 #define CE 5
 #define DC 4
 #define DIN 2
-#define CLK2 15
+#define lcdCLK 15
 //Nokia_5110 lcd = Nokia_5110(RST, CE, DC, DIN, CLK2);
-Adafruit_PCD8544 lcd = Adafruit_PCD8544(CLK2, DIN, DC, CE, RST); //RST, CE, DC, DIN, CLK2
+Adafruit_PCD8544 lcd = Adafruit_PCD8544(lcdCLK, DIN, DC, CE, RST); //RST, CE, DC, DIN, CLK2
 
 //load cell library
 #define CLK 25
 #define DOUT 33
+#define DOUT2 32
+#define DOUT3 35
+#define DOUT4 34
+
 HX711 scale;
+HX711 scale2;
+HX711 scale3;
+HX711 scale4;
 
 //variables for loadcell values
-float load;
+float load;     //asıl ağırlığımızı tuttuğumuz kısım
 float tempLoad; // gecici ağırlık değişkeni
 float difLoad;  // çıkan yada eklenen farkı tutan değişken
 
@@ -48,7 +55,7 @@ void sendSensor()
 
 BLYNK_WRITE(V0)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V0 to a variable
+  int pinValue = param.asInt();
   if (pinValue > 0)
   {
     tempLoad = load; // telefondaki sanal tuş basıldığında
@@ -57,65 +64,70 @@ BLYNK_WRITE(V0)
 
 void updateLcd(int row, int col, float val, const char *name)
 {
-  lcd.setCursor(col, row);
-  //lcd.clear(row, 0, 84);
-
-  lcd.printf("%s %.2f \n", name, val);
-
-  lcd.display();
+  lcd.setCursor(col, row);             //imleci istenen yere getirme
+  lcd.printf("%s %.2f \n", name, val); //gelen değerleri yazma
+  lcd.display();                       // değişiklikleri onaylama
 }
 
 void getLoad()
 {
-  //scale.read_average(10);
-  load = scale.get_units(10);
+  load = 0;
+  load = scale.get_units(5) + scale2.get_units(5) + scale3.get_units(5) + scale4.get_units(5); //4 hücreden 5er adet ortalama değerler okuyup toplamını load a yazıyoruz
 }
 
 void tare()
 {
-  scale.read_average(10);
-  delay(300);
-  scale.tare();
-  delay(300);
-  scale.read_average(10);
+  scale.read_average(10); // 10 adet değer okuyup ortalamasını alıyoruz
+  delay(100);             //değerlerin kararlı hale gelmesi için 100ms bekleme
+  scale.tare();           //dara alma işlemi gerçekleşiyor
+                          //yukarıdaki işlem diğer tüm hücreler için tekrarlanıyor
+  scale2.read_average(10);
+  delay(100);
+  scale2.tare();
+
+  scale3.read_average(10);
+  delay(100);
+  scale3.tare();
+
+  scale4.read_average(10);
+  delay(100);
+  scale4.tare();
 }
 
 void setup()
 {
   setCpuFrequencyMhz(80); //cpu hızını 80mhz e düşürüyoruz
-  //Serial.begin(115200);
+  Serial.begin(115200);   //bilgisayardan haberleşme için
 
-  pinMode(11, OUTPUT);
-  //loadcell okuma başlatılıyor
   scale.begin(DOUT, CLK);
-  //scale.read_average(20);
-  scale.set_scale(); // Kalibrasyon böleni tek loadcell=151.8
-  tare();
-  //scale.set_offset();
-  //scale.tare();           // dara alma komutu
-  //delay(1000);
-  //scale.read_average(20); // açılışta ortalama okuma yapma için
+  scale2.begin(DOUT2, CLK);
+  scale3.begin(DOUT3, CLK);
+  scale4.begin(DOUT4, CLK);
 
-  //lcd ekran konstrast değeri
-  lcd.begin();
-  lcd.setContrast(50);
-  lcd.setTextSize(1, 2);
+  scale.set_scale(165.78); // Kalibrasyon böleni her hücre için farklı olabilir bu nedenle tek tek hesaplanacak
+  scale2.set_scale(164.6);
+  scale3.set_scale(164.2);
+  scale4.set_scale(162.16);
+  tare(); // tüm hücrelerin darasını alan metod
 
-  //blynk bağlantısı başlıyor
-  Blynk.begin(auth, ssid, pass);
-  timer.setInterval(1000L, sendSensor);
+  lcd.begin();                          // lcd ekran başlatılıyor
+  lcd.setContrast(50);                  //lcd ekran konstrast değeri
+  lcd.setTextSize(1, 2);                // ekran yazı boyutu ayarı
+  lcd.clearDisplay();                   // açılışta ekranı temizleme
+  Blynk.begin(auth, ssid, pass);        //blynk bağlantısı başlıyor
+  timer.setInterval(1000L, sendSensor); //blynk için bir sayaç
 }
 
 void loop()
 {
-  getLoad();
-  updateLcd(0, 0, load, "YUK:");
-  difLoad = load - tempLoad;
-  updateLcd(20, 0, difLoad, "FARK:");
-  lcd.clearDisplay();
+  getLoad();                          //ağırlık değişkenini güncelleyen metod
+  updateLcd(0, 0, load, "YUK:");      // 0 - 0 koordinatlarından başlayarak lcd ekrana load değişkenini yazdırma
+  difLoad = load - tempLoad;          // eklenen yada çıkarılan değeri hesaplamak için
+  updateLcd(20, 0, difLoad, "FARK:"); //20 - 0 koordinatlarından başlayarak lcd ekrana load değişkenini yazdırma
+  lcd.clearDisplay();                 //gösterimler ardından ekranı temizlemek için
 
-  Blynk.run();
-  timer.run();
+  Blynk.run(); //blynk sistemini çalıştırıyoruz
+  timer.run(); //blynk sayacını çalıştırıyoruz
 
   if (touchRead(13) < 30)
   {
